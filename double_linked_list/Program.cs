a05bb2f2164f17d05af3ccaf080a630a8754f63e
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2022-11-18 16:49:49 +0700

    create class Node

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -2,6 +2,19 @@
 
 namespace double_linked_list
 {
+    class Node
+    {
+        /* Node class represent the node of doubly linked list
+         * it consist of the information part and links to
+         * its succeding and preceeding
+         * in terms of next and previous */
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        //point to the preceeding node
+        public Node prev;
+    }
     class Program
     {
 
